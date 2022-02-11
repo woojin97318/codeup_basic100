c31fe84d1e88595ed8e64c2999e4ede60882d7e0
woojin97318 <woojin97318@naver.com>
2022-02-11 16:52:31 +0900

    (20220211)코드업기초100제-1025번 푸는중

---
+++ src/codeup_basic100/q1020_1029/Q1022.java
@@ -0,0 +1,12 @@
+package codeup_basic100.q1020_1029;
+
+import java.util.Scanner;
+
+public class Q1022 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String str = sc.nextLine();
+		System.out.println(str);
+	}
+}

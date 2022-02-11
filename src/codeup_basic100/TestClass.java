34394101fa31acc51854bac3779a14ac4077c655
woojin97318 <woojin97318@naver.com>
2022-02-11 17:09:23 +0900

    (20220211)코드업기초100제-1025 풀이완료

--- src/codeup_basic100/TestClass.java
+++ src/codeup_basic100/TestClass.java
@@ -3,7 +3,6 @@ package codeup_basic100;
 public class TestClass {
 
 	public static void main(String[] args) {
-		int a = 10;
-		System.out.printf("%04d", a);
+		System.out.println(1/10);
 	}
 }
--- src/codeup_basic100/q1020_1029/Q1025.java
+++ src/codeup_basic100/q1020_1029/Q1025.java
@@ -6,7 +6,16 @@ public class Q1025 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt();
-		
+		int num = sc.nextInt(); //75254
+		for(int i = 10000; i != 0; i = i / 10) {
+			System.out.print("[");
+			System.out.print(num / i % 10 * i);
+			System.out.print("]\n");
+		}
+//		System.out.println(num / 10000 % 10 * 10000);
+//		System.out.println(num / 1000 % 10 * 1000);
+//		System.out.println(num / 100 % 10 * 100);
+//		System.out.println(num / 10 % 10 * 10);
+//		System.out.println(num /1 % 10 * 1);
 	}
 }

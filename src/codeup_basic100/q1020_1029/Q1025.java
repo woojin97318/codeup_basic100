27f2ec919bd6335896d0b90671aa895f38c0ba07
woojin97318 <woojin97318@naver.com>
2022-02-11 23:33:36 +0900

    (20220211)코드업기초100제-1027번 풀이완료

--- src/codeup_basic100/q1020_1029/Q1025.java
+++ src/codeup_basic100/q1020_1029/Q1025.java
@@ -6,16 +6,27 @@ public class Q1025 {
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		int num = sc.nextInt(); //75254
-		for(int i = 10000; i != 0; i = i / 10) {
-			System.out.print("[");
-			System.out.print(num / i % 10 * i);
-			System.out.print("]\n");
-		}
+
+		System.out.println("规过 1");
+		int num = sc.nextInt();
 //		System.out.println(num / 10000 % 10 * 10000);
 //		System.out.println(num / 1000 % 10 * 1000);
 //		System.out.println(num / 100 % 10 * 100);
 //		System.out.println(num / 10 % 10 * 10);
-//		System.out.println(num /1 % 10 * 1);
+//		System.out.println(num / 1 % 10 * 1);
+		for(int i = 10000; i != 0; i = i / 10) {
+			System.out.print("[");
+			System.out.print(num / i % 10 * i);
+			System.out.println("]");
+		}
+		
+		System.out.println("\n规过 2");
+		String str[] = sc.next().split("");
+		int cnt = str.length;
+		for(String s : str) {
+			System.out.print("[");
+			System.out.print((int)(Integer.parseInt(s) * (Math.pow(10, --cnt))));
+			System.out.println("]");
+		}
 	}
 }

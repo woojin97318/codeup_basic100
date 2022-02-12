6923bedf998e95f218eda0855fde6569af5b8844
woojin97318 <woojin97318@naver.com>
2022-02-12 22:30:43 +0900

    (20220212)코드업기초100제-1030번 풀이완료

---
+++ src/codeup_basic100/q1020_1029/Q1029.java
@@ -0,0 +1,13 @@
+package codeup_basic100.q1020_1029;
+
+import java.util.Scanner;
+
+public class Q1029 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		double num = sc.nextDouble();
+		System.out.println(num);
+	}
+}
---
+++ src/codeup_basic100/q1030_1039/Q1031.java
@@ -0,0 +1,8 @@
+package codeup_basic100.q1030_1039;
+
+public class Q1031 {
+
+	public static void main(String[] args) {
+		
+	}
+}
